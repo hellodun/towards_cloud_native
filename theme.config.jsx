eb2c0dad005fb595e5de2d5455d1e66f00cfd306
Duncan Gatuthu <dgatuthu@gmail.com>
2023-12-18 12:26:45 +0300

    added logo image space btwn it and site name

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,9 +1,9 @@
 export default {
     logo: (
         <>
-        <img src="https://github.com/hellodun/towards_cloud_native/blob/main/images/logo-v1-white.svg" alt="tcn logo" />
+        <img src="https://raw.githubusercontent.com/hellodun/towards_cloud_native/316bcaa5d46cec7e550a3ccfc9185e15f5af8478/images/logo-v1-white.svg" width="6%" height="6%" alt="tcn logo" style={{ marginRight: '20px' }}/>
         <br />
-        <span>towards Cloud Native</span>
+        <span  style={{ fontWeight: "bold" }} >towards Cloud Native</span>
         </>
       ),
     project: {
